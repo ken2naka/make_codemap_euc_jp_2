77c77dd4e25aaff67881eb7775af4b59165046b9
ken2naka <ken1.naka@gmail.com>
2019-02-01 22:28:24 +0900

    add some comments

--- mk_euc_map.py
+++ mk_euc_map.py
@@ -8,7 +8,8 @@
 作成：2018-09-13
 目的：
 　　・euc_jp の2バイトコード（全角漢字）のエリアマップの漢字部分
-　　　を "euc_code_table.txt" に出力（utf-8）する。
+　　　を標準出力に出力（utf-8）する。
+　　・同時に "euc_code_table2.text" というファイルにも出力する。
 　　・その際、「JIS X 0208 (1990) to Unicode 漢字コード表」に
 　　　存在しないコード部分は 0xfff0efbfbd （'�'）で埋める。
 備考：xyzzy やサクラエディタでは 0xfff0efbfbd （'�'）は正しく
